a8cc8701d70088673275b04ef8ff4267d9050f42
Grilindor <marcor91@hotmail.fr>
2024-04-08 09:31:55 +0000

    hot fix

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - function that creates a binary tree node
+ *@
+ *@value:
+ *
+ *
+ **/
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	
+}
