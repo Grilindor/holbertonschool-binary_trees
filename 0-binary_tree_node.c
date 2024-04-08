6c8bde61fe3380932dbf4718c1bf94138c9b9571
Grilindor <marcor91@hotmail.fr>
2024-04-08 12:38:30 +0000

    fix commentari and betty

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
 	if (new_node == NULL)
-		return NULL; // Return NULL on failure
+		return (NULL); /*Return NULL on failure*/
 
 	new_node->n = value;
 	new_node->parent = parent;
