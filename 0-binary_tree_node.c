3a6982e77931d19f4dd8d79a35c38931a770c926
Grilindor <marcor91@hotmail.fr>
2024-04-08 11:49:36 +0000

    change valu to n

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,13 +2,22 @@
 
 /**
  * binary_tree_node - function that creates a binary tree node
- *@
- *@value:
- *
- *
+ *@parent: is a pointer to the parent node of the node to create
+ *@value: is a pointer to the parent node of the node to create
+ *Return: new_node
  **/
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+
+	if (new_node == NULL)
+		return NULL; // Return NULL on failure
+
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return (new_node);
 }
